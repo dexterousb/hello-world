6143aea2841ce7058fd592e08c9c726a82388eec
dexterousb <84274083+dexterousb@users.noreply.github.com>
2021-05-19 16:25:11 +0530

    Add files via upload

---
+++ Q8.py
@@ -0,0 +1,7 @@
+# Q8) WAP to find all occurrences of “UK” in a given string ignoring the casev “I am so british I feel like going to the UK and living. I think uk is very prosperous country”
+
+#input
+string = "I am so british I feel like going to the UK and living. I think uk is very prosperous country"
+
+#to count substring
+print(string.count("UK"))

6a3e0a37812e77425481c87d229eeba9a4310980
dexterousb <84274083+dexterousb@users.noreply.github.com>
2021-05-19 19:07:45 +0530

    Add files via upload

---
+++ Q10.py
@@ -0,0 +1,8 @@
+#Q10) WAP to find the last position of a substring “Emma” in a given string: "Emma is a data scientist who knows Python. Emma works at google."
+
+
+#input
+string = "Emma is a data scientist who knows Python. Emma works at google"
+
+#to display occurence in substring
+print(string.rfind('a'))
